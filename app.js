ffae1064f93d6f9ecac28fa7733b95d68d554cf4
MrLeo7 <pillatok1@gmail.com>
2022-09-30 02:39:23 +0400

    added dashboard

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.engine("handlebars", exphbs.engine({ defaultLayout: "home" }));
 app.set('view engine', 'handlebars');
 
-const main = require('./routess/home/main')
-app.use('/',main)
+const home = require('./routess/home/main')
+const admin = require('./routess/admin/main')
+
+app.use('/',home)
+app.use('/admin',admin)
 app.listen(4001, () => {
   console.log("listening");
 });
