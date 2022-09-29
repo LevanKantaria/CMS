2a581a565d990548aea8f2e5171dd05ba241bf78
MrLeo7 <pillatok1@gmail.com>
2022-09-30 02:50:18 +0400

    added logit to dashboard components

--- app.js
+++ app.js
@@ -8,11 +8,21 @@ app.use(express.static(path.join(__dirname, "public")));
 app.engine("handlebars", exphbs.engine({ defaultLayout: "home" }));
 app.set('view engine', 'handlebars');
 
+
+
 const home = require('./routess/home/main')
 const admin = require('./routess/admin/main')
 
+
+
+
 app.use('/',home)
 app.use('/admin',admin)
+
+
+
+
+
 app.listen(4001, () => {
   console.log("listening");
 });
